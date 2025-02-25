6f35b4ff763a5883d6d19ec20924321bcafef089
Vladimir Herdman <vovaherdman@gmail.com>
2025-02-25 11:31:44 -0500

    Initial login and homescreen setup

--- app/src/main/java/com/zybooks/workway/CalendarActivity.kt
+++ app/src/main/java/com/zybooks/workway/CalendarActivity.kt
@@ -1,30 +1,20 @@
 package com.zybooks.workway
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_calendar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun onSignInClick(view: View){
-        val intent = Intent(this, HomeScreen::class.java)
-        startActivity(intent)
-        this.finish()
-        Log.i("Login", "Destroyed login page activity")
     }
 }
---
+++ app/src/main/java/com/zybooks/workway/HomeScreen.kt
@@ -0,0 +1,43 @@
+package com.zybooks.workway
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeScreen : AppCompatActivity() {
+    private lateinit var optionsMenu: Menu
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        optionsMenu = menu!!
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Determine which menu option was chosen
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                Log.i("AppBar", "Logout Button Pressed")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
