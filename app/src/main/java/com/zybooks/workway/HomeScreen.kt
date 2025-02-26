f6c5b5abbd365d6d799574a4a9449de27b745b14
Vladimir Herdman <vovaherdman@gmail.com>
2025-02-26 00:04:11 -0500

    Added BASIC fragment structure for different screens
    Different screens viewable by clicking the options in the bottom navigation menu

--- app/src/main/java/com/zybooks/workway/HomeScreen.kt
+++ app/src/main/java/com/zybooks/workway/HomeScreen.kt
@@ -6,8 +6,12 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeScreen : AppCompatActivity() {
     private lateinit var optionsMenu: Menu
@@ -20,6 +24,32 @@ class HomeScreen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Upper toolbar
+        val toolbar: MaterialToolbar = findViewById(R.id.myToolbar)
+        toolbar.elevation = 8f
+        toolbar.overflowIcon?.setTint(ContextCompat.getColor(this, R.color.white))
+        setSupportActionBar(toolbar)
+
+        // Bottom menu and fragment selections
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val trainingFragment = Training()
+        val meetingsFragment = Meetings()
+        val messagesFragment = Messages()
+        val homeFragment = Home()
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_training -> setCurrentFragment(trainingFragment)
+                R.id.action_meetings -> setCurrentFragment(meetingsFragment)
+                R.id.action_messages -> setCurrentFragment(messagesFragment)
+                R.id.action_home -> setCurrentFragment(homeFragment)
+            }
+            true
         }
     }
 
@@ -40,4 +70,11 @@ class HomeScreen : AppCompatActivity() {
         }
     }
 
+    private fun setCurrentFragment(fragment: Fragment) {
+        Log.i("Fragment", "Replacing with ${fragment.javaClass.simpleName}")
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commitAllowingStateLoss()
+        }
+    }
 }
