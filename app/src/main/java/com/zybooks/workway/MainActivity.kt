c09bf1f2630f597353f833888264a44d9a8682c3
Vladimir Herdman <vovaherdman@gmail.com>
2025-03-06 19:14:13 -0500

    Took off google login to ease sign in changes

--- app/src/main/java/com/zybooks/workway/MainActivity.kt
+++ app/src/main/java/com/zybooks/workway/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.zybooks.workway
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -79,6 +80,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSignInClick(view: View){
-        authManager.run()
+        // authManager.run()
+        startActivity(Intent(this, HomeScreen::class.java))
     }
 }
